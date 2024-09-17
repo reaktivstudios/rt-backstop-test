7ea62621d7823216555d4c4d3307e84f109d765b
Nick Croft <NicktheGeek@NickGeek.com>
2024-09-17 14:15:23 -0400

    replace images with placeholder

--- backstop/engine_scripts/puppet/onReady.js
+++ backstop/engine_scripts/puppet/onReady.js
@@ -1,6 +1,5 @@
 module.exports = async (page, scenario, a, b, c, data) => {
 	console.log('SCENARIO > ' + scenario.label);  
 	await require('./clickAndHoverHelper')(page, scenario, data);
-
 	// add more ready handlers here...
 };
